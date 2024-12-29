2dab6b87ce65f29675146aed984daf9e24c9b2d5
jophycui <jophycui@gmail.com>
2024-12-29 15:22:10 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to LeChinese!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          You account is now activated, go back to lechinese app and login.
         </p>
       </main>
 
